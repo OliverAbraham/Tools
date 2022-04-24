b207b4e5df44e198f33f8e84d3073783b4e23648
Oliver Abraham <mail@oliver-abraham.de>
2022-04-24 14:10:15 +0200

    GenerateTimestampEnvVar

---
+++ GenerateTimestampEnvVar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenerateTimestampEnvVar
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"set datestr={DateTime.Now.ToString("yyyy-MM-dd")}");
+			Console.WriteLine($"set timestr={DateTime.Now.ToString("HH-mm-ss")}");
+			Console.WriteLine($"set timestamp={DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}");
+		}
+	}
+}
