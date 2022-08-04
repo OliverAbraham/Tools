11f9ec868597f5b1ae7f8462a61bf60dba20c0d0
Oliver Abraham <mail@oliver-abraham.de>
2022-08-04 16:49:36 +0200

    DockerComposeHelper

---
+++ DockerComposeHelper/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("creates a CMD file to set an environment variable with the image of a container");
+Console.WriteLine("reading the file named 'temp'");
+
+var contents = File.ReadAllText("temp");
+var parts = contents.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+var command = $"set CONTAINER_ID={parts[0]}";
+File.WriteAllText("set_container_id.cmd", command);
+
+Console.WriteLine("created a CMD file named 'set_container_id.cmd'");
