6e25411327506b8f743f4f30f183257f361a7066
Oliver Abraham <mail@oliver-abraham.de>
2024-03-28 17:32:47 +0100

    ResetArchiveAttributes

--- ResetArchiveAttributes/ResetArchiveAttributes/Program.cs
+++ ResetArchiveAttributes/ResetArchiveAttributes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ResetArchiveAttributes
 {
@@ -10,39 +11,53 @@ namespace ResetArchiveAttributes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ResetArchiveAttributes    -   Oliver Abraham 2014");
+            Console.WriteLine("ResetArchiveAttributes    -   Oliver Abraham 2023");
             if (args.GetLength(0) < 1)
             {
                 Console.WriteLine("Verwendung: ResetArchiveAttributes {verzeichnisname}");
                 return;
             }
 
-            Console.WriteLine("Suche Dateien in Verzeichnis {0} ...", args[0]);
-            string[] Dateien = Directory.GetFiles(args[0], "*", SearchOption.AllDirectories);
+            Console.WriteLine($"Suche Dateien in Verzeichnis {args[0]} ...");
+            List<string> alleDateien = Directory.GetFiles(args[0], "*", SearchOption.AllDirectories).ToList();
+            Console.WriteLine($"Dateien gesamt: {alleDateien.Count}");
+            Console.WriteLine($"Suche nach Dateien mit Archivbit...");
 
-            UInt64 Zurückgesetzt = 0;
-            Console.WriteLine("Setze Archiv-Informationen zurück...");
-            foreach (string Datei in Dateien)
+            var dateienMitArchivbit = new List<string>();
+            Parallel.ForEach (alleDateien, delegate(string datei)
+                {
+                    try
+                    {
+                        FileAttributes Fa = File.GetAttributes(datei);
+                        if ((Fa & FileAttributes.Archive) != 0)
+                            dateienMitArchivbit.Add(datei);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("{0,50}':{1}\n", datei, ex.ToString().Replace("\n", "").Replace("\r", ""));
+                    }
+                });
+            Console.WriteLine($"Dateien mit Archivbit: {dateienMitArchivbit.Count}");
+            
+
+            Console.WriteLine($"Loesche Archiv-Informationen...");
+            UInt64 zähler = 0;
+            foreach (string datei in dateienMitArchivbit)
             {
                 try
                 {
-                    FileAttributes Fa = File.GetAttributes(Datei);
-                    if ((Fa & FileAttributes.Archive) != 0)
-                    {
-                        Fa = Fa & (~FileAttributes.Archive);  // FileAttributes.Archive löschen
-                        File.SetAttributes(Datei, Fa);
-                        Zurückgesetzt++;
-                    }
+                    FileAttributes Fa = File.GetAttributes(datei);
+                    Fa = Fa & (~FileAttributes.Archive);  // FileAttributes.Archive löschen
+                    File.SetAttributes(datei, Fa);
+                    zähler++;
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Fehler bei Verarbeitung von Datei '{0}':\n{1}\n\n", 
-                        Datei, ex.ToString());
+                    Console.WriteLine("{0,50}':{1}", datei, ex.Message.Replace("\n", "").Replace("\r", ""));
                 }
             }
             
-            Console.WriteLine("Fertig.  {0} Dateien verarbeitet, bei {1} Dateien das Archivbit zurückgesetzt.", 
-                Dateien.GetLength(0), Zurückgesetzt);
+            Console.WriteLine($"Fertig.  {alleDateien.Count} Dateien verarbeitet, bei {zähler} Dateien das Archivbit zurückgesetzt.");
         }
     }
 }
